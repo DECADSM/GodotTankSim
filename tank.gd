53c9ed39144021c352f95872cba19ab1634d8b5e
decadsm <duke129409@gmail.com>
2025-04-07 21:32:34 -0400

    Janky camera movement that rotates the cube

--- tank.gd
+++ tank.gd
@@ -4,25 +4,20 @@ class_name TankMovement
 
 @export var speed = 10 #This is the forward speed of the tank
 @export var rotational_speed = 5 #This is the rotational Speed of the tank
-var target_velocity = Vector3.ZERO
+@export var sensitivity = 0.01
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$"CameraPivot/Camera".rotation.y -= event.relative.x * sensitivity
+		$"CameraPivot/Camera".rotation.x -= event.relative.y * sensitivity
+		$Hull.rotation.y = $"CameraPivot/Camera".rotation.y
 
 func _physics_process(_delta):
-	var direction = Vector3.ZERO
-	if Input.is_action_pressed("MoveForward"):
-		direction.z += 1
-	if Input.is_action_pressed("MoveBack"):
-		direction.z -= 1
-	if Input.is_action_pressed("MoveRight"):
-		direction.x += 1
-	if Input.is_action_pressed("MoveLeft"):
-		direction.x -= 1
-
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Hull.basis = Basis.looking_at(direction)
-		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-	velocity = target_velocity
+	var input = Input.get_vector("ui_right", "ui_left", "ui_up", "ui_down")
+	var forward = $"CameraPivot/Camera".global_transform.basis.z
+	var right = $"CameraPivot/Camera".global_transform.basis.x
+	forward.y = 0
+	right.y = 0
+	velocity = (right.normalized() * input.x + forward.normalized() * input.y) * speed
 	move_and_slide()
 	#move_and_collide()
