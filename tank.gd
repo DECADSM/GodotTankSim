3004704ac5fd1fc5f14e250abf4c05398623a19f
decadsm <duke129409@gmail.com>
2025-04-08 23:40:51 -0400

    mouse locked in screen, mouse moves camera up and down and sidew to sode moves turret

--- tank.gd
+++ tank.gd
@@ -4,20 +4,24 @@ class_name TankMovement
 
 @export var speed = 10 #This is the forward speed of the tank
 @export var rotational_speed = 5 #This is the rotational Speed of the tank
-@export var sensitivity = 0.01
+@export var sensitivity = 0.005
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		$"CameraPivot/Camera".rotation.y -= event.relative.x * sensitivity
-		$"CameraPivot/Camera".rotation.x -= event.relative.y * sensitivity
-		$Hull.rotation.y = $"CameraPivot/Camera".rotation.y
+		$Hull/Turret.rotation.y -= event.relative.x * sensitivity
+		$Hull/Turret/CameraPivot/Camera.rotation.x -= event.relative.y * sensitivity
+		#$Hull.rotation.y = $"CameraPivot/Camera".rotation.y
+	if Input.is_action_pressed("Escape"):
+		get_tree().quit()
 
 func _physics_process(_delta):
-	var input = Input.get_vector("ui_right", "ui_left", "ui_up", "ui_down")
-	var forward = $"CameraPivot/Camera".global_transform.basis.z
-	var right = $"CameraPivot/Camera".global_transform.basis.x
+	var input = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBack")
+	var forward = $"Hull".global_transform.basis.z
 	forward.y = 0
-	right.y = 0
-	velocity = (right.normalized() * input.x + forward.normalized() * input.y) * speed
+	$Hull.rotate()
+	velocity = ( forward.normalized() * input.y) * speed
 	move_and_slide()
 	#move_and_collide()
